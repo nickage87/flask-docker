938d8f1356fa48f4f450ca2ef29526a86a746661
Nikolay Sizov <nikolay.sizov@cxense.com>
2017-09-12 03:50:26 -0400

    Added container run and Dockdr file tracker

--- flask.pp
+++ flask.pp
@@ -1,8 +1,25 @@
 node default {
 
+#vcsrepo { 'flask-app':
+#	ensure   => present,
+#	provider => git,
+#	source   => 'git@github.com:nickage87/flask-docker.git',
+#}
 docker::image {'nickage87/flask-app:1.0':
         docker_file     => '/root/flask-docker/Dockerfile',
-        #subscribe      => File['/root/flask-docker/Dockerfile'],
+        subscribe      => File['/root/Dockerfile'],
+}
+file {'/root/Dockerfile':
+	source => "https://github.com/nickage87/flask-docker/blob/master/Dockerfile",
+}
+docker::run {'flask-app-1.0':
+	image           => 'nickage87/flask-app:1.0',
+	restart_service => true,
+	privileged      => false,
+	pull_on_start   => false,
+	ports           => [ '80:80' ],
+	expose          => [ '80' ],
+
 }
 
 #include docker
