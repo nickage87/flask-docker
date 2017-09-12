13f1559f6d83876f30e34d4764120760bce6c4eb
Nikolay Sizov <nikolay.sizov@cxense.com>
2017-09-12 05:15:42 -0400

    Working file tracking was added

--- flask.pp
+++ flask.pp
@@ -1,33 +1,35 @@
 node default {
 
-#vcsrepo { 'flask-app':
-#	ensure   => present,
-#	provider => git,
-#	source   => 'git@github.com:nickage87/flask-docker.git',
-#}
+vcsrepo { '/root/flask-docker':
+        ensure   => present,
+        provider => git,
+        source   => 'https://github.com/nickage87/flask-docker.git',
+}
+vcsrepo { '/root/flask-app':
+        ensure   => present,
+        provider => git,
+        source   => 'https://github.com/nickage87/flask-app.git',
+}
 docker::image {'nickage87/flask-app:1.0':
-        docker_file     => '/root/flask-docker/Dockerfile',
+        docker_file     => '/root/Dockerfile',
         subscribe      => [
-				File['/root/Dockerfile'],
-				File['/root/flaskapp.py'],
-			],
+                                File['/root/Dockerfile'],
+                                File['/root/flaskapp.py'],
+                        ],
 }
 file {'/root/Dockerfile':
-	source	=> "https://raw.githubusercontent.com/nickage87/flask-docker/master/Dockerfile",
+        source  =>      "file:///root/flask-docker/Dockerfile",
 }
 file {'/root/flaskapp.py':
-	source	=> "https://raw.githubusercontent.com/nickage87/flask-app/master/flaskapp.py", 
+        source  =>      "file:///root/flask-app/flaskapp.py",
 }
 docker::run {'flask-app-1.0':
-	image           => 'nickage87/flask-app:1.0',
-	restart_service => true,
-	privileged      => false,
-	pull_on_start   => false,
-	ports           => [ '80:80' ],
-	expose          => [ '80' ],
+        image           => 'nickage87/flask-app:1.0',
+        restart_service => false,
+        privileged      => false,
+        pull_on_start   => true,
+        ports           => [ '80:80' ],
+        expose          => [ '80' ],
 
 }
-
-#include docker
-
 }
