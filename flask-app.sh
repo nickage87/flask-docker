0ac8472d8f2b75a75d4fffd3b9db50bfbca521a2
root <root@fedora.local>
2017-09-12 06:12:46 -0400

    Updated IP addres variable

--- flask-app.sh
+++ flask-app.sh
@@ -4,7 +4,7 @@ case $1 in
         "boot") aws ec2 run-instances --image-id ami-10547475 --count 1 --instance-type t2.micro --key-name flask-app-key --security-group-ids sg-c5a908ad --subnet-id subnet-8677c4cb --user-data file://user_data.sh ;;
         "update") if echo $2 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b";
                         then
-                                ssh ubuntu@$ipaddress -i flask-app-key.pem "/bin/bash ./update.sh" 
+                                ssh ubuntu@$2 -i flask-app-key.pem "/bin/bash ./update.sh" 
                 else
                         echo "Please verify your IP address!"
                 fi
