d594ec683d430048caf7ac85f867957b52f85d04
Nikolay Sizov <nikolay.sizov@cxense.com>
2017-09-12 06:06:06 -0400

    updated path to file

--- flask-app.sh
+++ flask-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 case $1 in
-        "boot") aws ec2 run-instances --image-id ami-10547475 --count 1 --instance-type t2.micro --key-name flask-app-key --security-group-ids sg-c5a908ad --subnet-id subnet-8677c4cb --user-data user_data.sh ;;
+        "boot") aws ec2 run-instances --image-id ami-10547475 --count 1 --instance-type t2.micro --key-name flask-app-key --security-group-ids sg-c5a908ad --subnet-id subnet-8677c4cb --user-data file://user_data.sh ;;
         "update") if echo $2 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b";
                         then
                                 ssh ubuntu@$ipaddress -i flask-app-key.pem "/bin/bash ./update.sh" 
